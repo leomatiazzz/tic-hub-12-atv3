d07efb6d307443e1a28db3c2a76b0d298f7b285a
leomatiazzz <leo21matias@hotmail.com>
2026-03-17 02:14:38 -0300

    refactor: remover estrutura duplicada e preparar para integração Vue

--- src/enum/shipment.ts
+++
@@ -1,7 +0,0 @@
-export enum Shipment {
-   FREE = "FREE",
-   CORREIOS = "CORREIOS",
-   PAC = "PAC",
-   SEDEX = "SEDEX",
-   AMAZON = "AMAZON",
-}
--- src/model/category.model.ts
+++
@@ -1,8 +0,0 @@
-export class Category {
-    constructor(
-        public id: string,
-        public title: string,
-        public description: string = "",
-        public isActive: boolean = true,
-    ){};
-}
